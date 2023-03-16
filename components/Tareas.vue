ae42d31dfc246bd7e641a585810cce45d6fef0d5
Natasha <105201801+natashanu@users.noreply.github.com>
2023-03-16 16:00:35 +0100

    Añadiendo funcionalidades a inputs

--- components/Tareas.vue
+++ components/Tareas.vue
@@ -1,19 +1,67 @@
 <template>
     <div>
-        <h1>Hola!</h1>
-        <h2>Aquí están mis tareas por hacer</h2>
+      <input type="text" v-model="newToDo">
+      <button v-on:click="addToDo">Agregar tarea</button>
+      <ul>
+        <div class="lista" v-for="tarea in tareas" :key="tarea.id">
+          <li>
+            <label :for="tarea.id">{{ tarea.descripcion }}</label>
+            <input type="checkbox" :id="tarea.id" :value="tarea.id" v-model="checkedToDo">
+          </li>
+        </div>
+      </ul>
+      <p>Tengo {{ count }} tareas que realizar</p>
+      <button v-if="checkedToDo && checkedToDo.length>0" v-on:click="removeToDo">Eliminar tareas</button>
     </div>
 
 </template>
 
 <script>
+let idElemento = 0;
+
 export default {
   name: 'Tareas',
   components:{
 
+  },
+  data(){
+    return{
+      newToDo:"",
+      tareas: [
+
+      ],
+      count : 0, 
+      checkedToDo: []
+    }
+  },
+  methods:{
+    addToDo: function(){
+      if(this.newToDo){
+        this.tareas.push({
+          id: idElemento,
+          descripcion: this.newToDo.trim()
+        })
+        this.newToDo = "";
+        this.count ++
+        idElemento++
+      }
+      
+    },
+    removeToDo: function(){
+      this.checkedToDo.forEach(element => {
+        this.tareas = this.tareas.filter(function(item) {
+          return item.id !== element
+        })  
+        this.count = this.tareas.length
+        
+      });
+    }
   }
 }
 </script>
 
 <style scoped>
+  ul{
+    list-style: none;
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <button>Un botton</button>
+  <div class="principal">
+    <h1>¡Hola!</h1>
+    <h2>Aquí están mis tareas por hacer</h2>
     <Tareas/>
   </div>
 </template>
 
 <script>
-import Tareas from "~/components/Tareas.vue";
+import Tareas from "~/components/Tareas";
+
 
 export default {
   name: 'IndexPage',
   components:{
     Tareas
-  }
+  }, 
 }
 </script>
+
+<style>
+  .principal{
+    text-align: center;
+    width: fit-content;
+    margin: auto;
+  }
+</style>
