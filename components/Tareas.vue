a7e46e1d6600e93b1db20d8b6d41ad1f3ab82961
Natasha <105201801+natashanu@users.noreply.github.com>
2023-03-16 07:56:09 +0100

    Primer commit

---
+++ components/Tareas.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hola!</h1>
+        <h2>Aquí están mis tareas por hacer</h2>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Tareas',
+  components:{
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>Un botton</button>
+    <Tareas/>
+  </div>
+</template>
+
+<script>
+import Tareas from "~/components/Tareas.vue";
+
+export default {
+  name: 'IndexPage',
+  components:{
+    Tareas
+  }
+}
+</script>
